8e5f290f72e9a189d4bf7a3ecf4bc84d5e2c890c
Daniel Santana <daniel_santana_87@hotmail.com>
2024-08-02 15:17:19 -0300

    Inserindo idiomas

--- ptf/ac.js
+++ ptf/ac.js
@@ -1,12 +1,22 @@
-// Adicionar um evento de clique ao botão do acordeão
+// Variável para armazenar o acordeão aberto
+let accordionOpen = null;
+
 var accordionButtons = document.querySelectorAll('.accordion-button');
 
 accordionButtons.forEach(function(button) {
-  button.addEventListener('click', function() {
-    this.classList.toggle('active');
-    var content = this.nextElementSibling;
+    button.addEventListener('click', function() {
+        // Verificar se já existe um acordeão aberto
+        if (accordionOpen) {
+            accordionOpen.classList.remove('active');
+            accordionOpen.nextElementSibling.classList.add('hidden');
+        }
 
-    // Alterne a classe "hidden" para mostrar/ocultar o conteúdo
-    content.classList.toggle('hidden');
-  });
+        // Atualizar o acordeão aberto
+        accordionOpen = this;
+
+        // Toggle da classe 'active' e mostrar/ocultar o conteúdo
+        this.classList.toggle('active');
+        var content = this.nextElementSibling;
+        content.classList.toggle('hidden');
+    });
 });
