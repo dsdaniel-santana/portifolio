a828139f6fada2ae799c84fdd338545b4fbbe1df
Daniel Santana <daniel_santana_87@hotmail.com>
2024-08-03 14:43:46 -0300

    Inserindo formacao e melhorando acordion

--- ptf/ac.js
+++ ptf/ac.js
@@ -5,18 +5,25 @@ var accordionButtons = document.querySelectorAll('.accordion-button');
 
 accordionButtons.forEach(function(button) {
     button.addEventListener('click', function() {
-        // Verificar se já existe um acordeão aberto
-        if (accordionOpen) {
-            accordionOpen.classList.remove('active');
-            accordionOpen.nextElementSibling.classList.add('hidden');
-        }
-
-        // Atualizar o acordeão aberto
-        accordionOpen = this;
-
-        // Toggle da classe 'active' e mostrar/ocultar o conteúdo
-        this.classList.toggle('active');
         var content = this.nextElementSibling;
-        content.classList.toggle('hidden');
+
+        // Verificar se já existe um acordeão aberto e se é o mesmo que está sendo clicado
+        if (accordionOpen && accordionOpen === this) {
+            // Fechar o acordeão aberto
+            accordionOpen.classList.remove('active');
+            content.classList.add('hidden');
+            accordionOpen = null;  // Resetar a variável
+        } else {
+            // Fechar o acordeão aberto anterior, se houver
+            if (accordionOpen) {
+                accordionOpen.classList.remove('active');
+                accordionOpen.nextElementSibling.classList.add('hidden');
+            }
+
+            // Abrir o novo acordeão
+            this.classList.toggle('active');
+            content.classList.toggle('hidden');
+            accordionOpen = this;  // Atualizar a variável para o novo acordeão aberto
+        }
     });
 });
